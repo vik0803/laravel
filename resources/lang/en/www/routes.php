59d6cd9f8fb02ba45d0d928969835eb3af5308d0
Dimitar Zlatev <zlatevbg@gmail.com>
2015-06-13 18:24:24 +0300

    Update www subdomain with LoginController and localized routes

--- resources/lang/en/www/routes.php
+++ resources/lang/en/www/routes.php
@@ -2,17 +2,24 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Routes
-	|--------------------------------------------------------------------------
-	|
-	| Routes array
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | CMS Routes
+    |--------------------------------------------------------------------------
+    |
+    | Routes array
+    |
+    */
 
-    'home' => 'home',
-    'auth/login' => 'auth/login',
-	'auth/register' => 'auth/register',
+    '/' => ['slug' => '', 'get' => 'LoginController@show', 'post' => 'LoginController@login'],
+    'logout' => ['slug' => 'logout', 'get' => 'LoginController@logout'],
+    'home' => ['slug' => 'home', 'get' => 'PageController@home'],
+    'page' => ['slug' => 'page', 'get' => 'PageController@show'],
+    'pf' => ['slug' => 'pf', 'get' => 'PasswordController@show', 'post' => 'PasswordController@pf', '/' => [
+        'mail' => ['slug' => 'mail', 'get' => 'PageController@showSubpage'],
+        'reset' => ['slug' => 'reset', 'get' => 'PageController@getReset'],
+        ]
+    ],
+    'reset' => ['slug' => 'reset', 'get' => 'PasswordController@getReset', 'post' => 'PasswordController@postReset'],
 
 ];
