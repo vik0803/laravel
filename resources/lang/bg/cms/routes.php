01f5ec16bd8443f6363e413cc16a00939d865102
Dimitar Zlatev <zlatevbg@gmail.com>
2015-06-15 18:22:26 +0300

    Include 'id' identifier in routes.php

--- resources/lang/bg/cms/routes.php
+++ resources/lang/bg/cms/routes.php
@@ -11,12 +11,12 @@ return [
     |
     */
 
-    '/' => ['slug' => '', 'get' => 'AuthController@getLogin', 'post' => 'AuthController@postLogin'],
-    'logout' => ['slug' => 'изход', 'get' => 'AuthController@getLogout'],
-    'home' => ['slug' => 'начало', 'get' => 'PageController@home'],
-    'page' => ['slug' => 'страница', 'get' => 'PageController@page'],
-    'pf' => ['slug' => 'забравена-парола', 'get' => 'PasswordController@getEmail', 'post' => 'PasswordController@postEmail'],
-    'reset' => ['slug' => 'възстанови-парола', 'get' => ['controller' => 'PasswordController@getReset', 'parameters' => '{token}'], 'post' => 'PasswordController@postReset'],
-    'register' => ['slug' => 'регистрация', 'get' => 'AuthController@getRegister', 'post' => 'AuthController@postRegister'],
+    '/' => ['id' => 1, 'slug' => '', 'get' => 'AuthController@getLogin', 'post' => 'AuthController@postLogin'],
+    'logout' => ['id' => 2, 'slug' => 'изход', 'get' => 'AuthController@getLogout'],
+    'home' => ['id' => 3, 'slug' => 'начало', 'get' => 'PageController@home'],
+    'page' => ['id' => 4, 'slug' => 'страница', 'get' => 'PageController@page'],
+    'pf' => ['id' => 5, 'slug' => 'забравена-парола', 'get' => 'PasswordController@getEmail', 'post' => 'PasswordController@postEmail'],
+    'reset' => ['id' => 6, 'slug' => 'възстанови-парола', 'get' => ['controller' => 'PasswordController@getReset', 'parameters' => '{token}'], 'post' => 'PasswordController@postReset'],
+    'register' => ['id' => 7, 'slug' => 'регистрация', 'get' => 'AuthController@getRegister', 'post' => 'AuthController@postRegister'],
 
 ];
--- resources/lang/en/cms/routes.php
+++ resources/lang/en/cms/routes.php
@@ -11,12 +11,12 @@ return [
     |
     */
 
-    '/' => ['slug' => '', 'get' => 'AuthController@getLogin', 'post' => 'AuthController@postLogin'],
-    'logout' => ['slug' => 'logout', 'get' => 'AuthController@getLogout'],
-    'home' => ['slug' => 'home', 'get' => 'PageController@home'],
-    'page' => ['slug' => 'page', 'get' => 'PageController@page'],
-    'pf' => ['slug' => 'pf', 'get' => 'PasswordController@getEmail', 'post' => 'PasswordController@postEmail'],
-    'reset' => ['slug' => 'reset', 'get' => ['controller' => 'PasswordController@getReset', 'parameters' => '{token}'], 'post' => 'PasswordController@postReset'],
-    'register' => ['slug' => 'register', 'get' => 'AuthController@getRegister', 'post' => 'AuthController@postRegister'],
+    '/' => ['id' => 1, 'slug' => '', 'get' => 'AuthController@getLogin', 'post' => 'AuthController@postLogin'],
+    'logout' => ['id' => 2, 'slug' => 'logout', 'get' => 'AuthController@getLogout'],
+    'home' => ['id' => 3, 'slug' => 'home', 'get' => 'PageController@home'],
+    'page' => ['id' => 4, 'slug' => 'page', 'get' => 'PageController@page'],
+    'pf' => ['id' => 5, 'slug' => 'pf', 'get' => 'PasswordController@getEmail', 'post' => 'PasswordController@postEmail'],
+    'reset' => ['id' => 6, 'slug' => 'reset', 'get' => ['controller' => 'PasswordController@getReset', 'parameters' => '{token}'], 'post' => 'PasswordController@postReset'],
+    'register' => ['id' => 7, 'slug' => 'register', 'get' => 'AuthController@getRegister', 'post' => 'AuthController@postRegister'],
 
 ];
