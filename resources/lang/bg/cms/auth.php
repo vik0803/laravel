ca375afc58503baabb4c114747e9d0045987ab7d
Dimitar Zlatev <zlatevbg@gmail.com>
2015-07-17 18:57:31 +0300

    Test Language Translation

---
+++ resources/lang/bg/cms/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Auth
+	|--------------------------------------------------------------------------
+	|
+	| Auth language file
+	|
+	*/
+
+    // Titles
+    'signInTitle' => 'Вход',
+    'resetPasswordTitle' => 'Reset Password',
+    'registerTitle' => 'Register',
+
+    'failedLogin' => 'These credentials do not match our records.',
+    'passwordForgottenHelpText' => 'I cannot access my account.',
+
+];
