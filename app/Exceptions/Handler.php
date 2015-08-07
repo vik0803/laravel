ce86c65cd0f967251d67f6255e0cf528494308ef
Dimitar Zlatev <zlatevbg@gmail.com>
2015-08-07 17:37:01 +0300

    Update TokenMismatchException Logic

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,13 +40,8 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
         if ($e instanceof \Illuminate\Session\TokenMismatchException) {
-            if ($request->ajax()) {
-                $request->flashExcept('_token');
-                $request->session()->flash('errors', new \Illuminate\Support\MessageBag([trans('validation.tokenMismatchException')]));
-                return response()->json(['redirect' => $request->fullUrl()]);
-            } else {
-                return redirect()->back()->withInput(\Input::except('_token'))->withErrors([trans('validation.tokenMismatchException')]);
-            }
+            // redirect back with error message does not work when session is expired, because the session is regenrated 2 times.
+            // instead I've overwritten the Illuminate\Foundation\Http\Middleware\VerifyCsrfToken handle method in the VerifyCsrfToken middleware.
         }
 
         return parent::render($request, $e);
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,36 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * I could have used the App\Exceptions\Handler.php render method, but...
+     *
+     * ...redirect back with error message does not work when session is expired,
+     * because the session is regenrated 2 times...
+     *
+     * ...so I overwrite the BaseVerifier handle method instead.
+     *
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed or redirects back with error (TokenMismatchException)
+     *
+     */
+    public function handle($request, \Closure $next)
+    {
+        if ($this->isReading($request) || $this->shouldPassThrough($request) || $this->tokensMatch($request)) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        // throw new TokenMismatchException;
+
+        if ($request->ajax()) {
+            $request->flashExcept('_token');
+            $request->session()->flash('errors', new \Illuminate\Support\MessageBag([trans('validation.tokenMismatchException')]));
+            return response()->json(['redirect' => $request->fullUrl()]);
+        } else {
+            return redirect()->to($request->fullUrl())->withInput(\Input::except('_token'))->withErrors([trans('validation.tokenMismatchException')]);
+        }
+    }
 }
