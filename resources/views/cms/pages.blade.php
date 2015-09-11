54629d0447ee3583742dcebeb590c81eb7e500ee
Dimitar Zlatev <zlatevbg@gmail.com>
2015-09-11 09:51:36 +0300

    Change 'pages' template

---
+++ resources/views/cms/pages.blade.php
@@ -0,0 +1,11 @@
+<?php
+$metaName = 'Page Name: Pages';
+$metaTitle = 'Page Title: Pages';
+$metaDescription = 'Page Description: Pages';
+?>
+
+@extends('cms.master')
+
+@section('content')
+    Pages here...
+@endsection
