50eba2d86f512f2e7487993be68be613c4ba42e6
Dimitar Zlatev <zlatevbg@gmail.com>
2015-09-11 16:37:48 +0300

    Update laravel/framework to v. 5.1.16

--- app/Services/Cms/Auth/AuthenticatesUsers.php
+++ app/Services/Cms/Auth/AuthenticatesUsers.php
@@ -5,7 +5,6 @@ namespace App\Services\Cms\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
-use Illuminate\Support\Facades\Cache;
 
 trait AuthenticatesUsers
 {
