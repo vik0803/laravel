4e74f170be3e61c203f821a41ed85b3d899c0e10
Dimitar Zlatev <zlatevbg@gmail.com>
2015-10-10 13:29:33 +0300

    Add ViewComposer and re-work breadcrumbs

---
+++ app/Services/ViewComposer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Contracts\View\View;
+
+class ViewComposer
+{
+    /**
+     * Creates new instance.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('jsCookies', isset($_COOKIE['jsCookies']) ? json_decode($_COOKIE['jsCookies'], true) : []);
+        $view->with('slug', \Slug::getRouteSlug());
+        $view->with('slugs', \Slug::getRouteSlugs());
+        $view->with('breadcrumbs', \Locales::createBreadcrumbsFromSlugs());
+    }
+}
