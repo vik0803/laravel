e5089819bb911c52f86b285a13f8952016418abe
Dimitar Zlatev <zlatevbg@gmail.com>
2015-11-11 11:37:32 +0200

    Add roles to users

---
+++ database/migrations/2015_11_09_110719_add_roles_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRolesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('role_id')->unsigned();
+            $table->index('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_role_id_foreign');
+            $table->dropIndex('users_role_id_index');
+            $table->dropColumn('role_id');
+        });
+    }
+}
