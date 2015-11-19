5cccf59e829c4d8dfe62a30207176924f74d4ea8
Dimitar Zlatev <zlatevbg@gmail.com>
2015-11-19 10:02:44 +0200

    Datatables set header automatically

--- resources/views/cms/partials/datatables.blade.php
+++ resources/views/cms/partials/datatables.blade.php
@@ -1,23 +1,13 @@
 @foreach($datatables as $id => $table)
     @foreach($table['buttons'] as $button)
     <div class="btn-group">
-        <a data-table="{{ $id }}" href="{{ $button['url'] }}" class="btn {{ $button['class'] }}">
+        <a @if ($multipleDatatables)data-table="{{ $id }}"@endif href="{{ $button['url'] }}" class="btn {{ $button['class'] }}">
             @if ($button['icon'])<span class="glyphicon glyphicon-{{ $button['icon'] }}"></span>@endif
             {{ $button['name'] }}
         </a>
     </div>
     @endforeach
     <div class="dataTableWrapper table-responsive ajax-lock" data-ajax-queue="async-{{ $id }}">
-        <table id="datatable{{ $id }}" class="dataTable table {{ $table['class'] }}">
-        @if ($table['ajax'])
-            <thead>
-            @foreach($table['columns'] as $column)
-                <th>{{ trans('cms/datatables.' . $column) }}</th>
-            @endforeach
-            </thead>
-            <tbody>
-            </tbody>
-        @endif
-        </table>
+        <table id="datatable{{ $id }}" class="dataTable table {{ $table['class'] }}"></table>
     </div>
 @endforeach
