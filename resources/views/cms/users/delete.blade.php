4567e34983f42477e600fbaef4e422679dbb9f28
Dimitar Zlatev <zlatevbg@gmail.com>
2015-11-25 01:45:50 +0200

    Rename a view

--- resources/views/cms/users/delete.blade.php
+++ resources/views/cms/users/delete.blade.php
@@ -6,7 +6,7 @@
 
     @include('cms/shared.errors')
 
-    {!! Form::open(['method' => 'delete', 'url' => \Locales::route('users/destroy'), 'id' => 'destroy-user-form', 'data-ajax-queue' => 'sync', 'class' => 'ajax-lock', 'role' => 'form']) !!}
+    {!! Form::open(['method' => 'delete', 'url' => \Locales::route('users/destroy'), 'id' => 'delete-user-form', 'data-ajax-queue' => 'sync', 'class' => 'ajax-lock', 'role' => 'form']) !!}
     {!! Form::hidden('table', $table, ['id' => 'input-table']) !!}
     {!! Form::submit(trans('cms/forms.destroyButton'), ['class' => 'btn btn-danger btn-block']) !!}
     {!! Form::close() !!}
