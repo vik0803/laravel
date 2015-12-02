44925f4a7900337d86ca8754e7b0949aedfc47d6
Dimitar Zlatev <zlatevbg@gmail.com>
2015-12-02 21:49:51 +0200

    Improve breadcrumb partial

--- resources/views/cms/partials/breadcrumbs.blade.php
+++ resources/views/cms/partials/breadcrumbs.blade.php
@@ -2,9 +2,10 @@
     <ol>
         <li class="nav-toggle{!! isset($jsCookies['navState']) ? ' collapsed' : '' !!}"><a href="#"><span class="glyphicon glyphicon-menu-hamburger"></span></a></li>
         @foreach (\Locales::createBreadcrumbsFromSlugs() as $breadcrumb)
-            <li{!! $breadcrumb['last'] ? ' class="active"' : '' !!}>
-                <a href="{{ $breadcrumb['link'] }}">{{ $breadcrumb['name'] }}</a>{!! !$breadcrumb['last'] ? '<span class="glyphicon glyphicon-chevron-right"></span>' : '' !!}
-            </li>
+            <li{!! $breadcrumb['last'] ? ' class="active"' : '' !!}><a href="{{ $breadcrumb['link'] }}">{{ $breadcrumb['name'] }}</a></li>
+            @if (!$breadcrumb['last'])
+                <li class="separator"><span class="glyphicon glyphicon-chevron-right"></span></li>
+            @endif
         @endforeach
     </ol>
 </div>
