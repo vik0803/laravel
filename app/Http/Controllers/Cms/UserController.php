b52d56997679685d0c35d2865506aa0a12718d92
Dimitar Zlatev <zlatevbg@gmail.com>
2015-12-02 21:51:53 +0200

    UserController use the new DataTable Service

--- app/Http/Controllers/Cms/UserController.php
+++ app/Http/Controllers/Cms/UserController.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use App\Role;
 use Validator;
+use App\Services\DataTable;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\EditUserRequest;
@@ -16,16 +17,8 @@ class UserController extends Controller {
 	|--------------------------------------------------------------------------
 	*/
 
-    public $datatables;
-
-    /**
-     * Single or Multiple DataTables on one page.
-     *
-     * @var string
-     */
-    protected $multipleDatatables = true;
-
     protected $route = 'users';
+    protected $datatables;
 
     /**
      * Create a new controller instance.
@@ -35,42 +28,13 @@ class UserController extends Controller {
     public function __construct()
     {
         $this->datatables = [
-            $this->route => [
-                'class' => 'table-checkbox table-striped table-bordered table-hover',
-                'checkbox' => true,
-                'columns' => [
-                    ['id' => 'name', 'name' => trans('cms/datatables.name')],
-                    ['id' => 'email', 'name' => trans('cms/datatables.email')],
-                ],
-                'orderByColumn' => 0,
-                'order' => 'asc',
-                'buttons' => [
-                    [
-                        'url' => \Locales::route($this->route . '/create'),
-                        'class' => 'btn-primary js-create',
-                        'icon' => 'plus',
-                        'name' => trans('cms/forms.createButton')
-                    ],
-                    [
-                        'url' => \Locales::route($this->route . '/edit'),
-                        'class' => 'btn-warning disabled js-edit',
-                        'icon' => 'edit',
-                        'name' => trans('cms/forms.editButton')
-                    ],
-                    [
-                        'url' => \Locales::route($this->route . '/delete'),
-                        'class' => 'btn-danger disabled js-destroy',
-                        'icon' => 'trash',
-                        'name' => trans('cms/forms.deleteButton')
-                    ],
-                ],
-            ],
             'admins' => [
+                'url' => \Locales::route($this->route, true),
                 'class' => 'table-checkbox table-striped table-bordered table-hover',
                 'checkbox' => true,
                 'columns' => [
-                    ['id' => 'name', 'name' => trans('cms/datatables.name')],
-                    ['id' => 'email', 'name' => trans('cms/datatables.email')],
+                    ['id' => 'name', 'name' => trans('cms/datatables.name'), 'search' => true],
+                    ['id' => 'email', 'name' => trans('cms/datatables.email'), 'search' => true],
                 ],
                 'orderByColumn' => 0,
                 'order' => 'asc',
@@ -93,14 +57,15 @@ class UserController extends Controller {
                         'icon' => 'trash',
                         'name' => trans('cms/forms.deleteButton')
                     ],
-                ],
+                ]
             ],
             'operators' => [
-                'class' => '',
-                'checkbox' => false,
+                'url' => \Locales::route($this->route, true),
+                'class' => 'table-checkbox table-striped table-bordered table-hover',
+                'checkbox' => true,
                 'columns' => [
-                    ['id' => 'email', 'name' => trans('cms/datatables.email')],
-                    ['id' => 'name', 'name' => trans('cms/datatables.name')],
+                    ['id' => 'name', 'name' => trans('cms/datatables.name'), 'search' => true],
+                    ['id' => 'email', 'name' => trans('cms/datatables.email'), 'search' => true],
                 ],
                 'orderByColumn' => 0,
                 'order' => 'asc',
@@ -111,25 +76,52 @@ class UserController extends Controller {
                         'icon' => 'plus',
                         'name' => trans('cms/forms.createButton')
                     ],
-                ],
+                    [
+                        'url' => \Locales::route($this->route . '/edit'),
+                        'class' => 'btn-warning disabled js-edit',
+                        'icon' => 'edit',
+                        'name' => trans('cms/forms.editButton')
+                    ],
+                    [
+                        'url' => \Locales::route($this->route . '/delete'),
+                        'class' => 'btn-danger disabled js-destroy',
+                        'icon' => 'trash',
+                        'name' => trans('cms/forms.deleteButton')
+                    ],
+                ]
             ],
+            $this->route => [
+                'url' => \Locales::route($this->route),
+                'class' => 'table-checkbox table-striped table-bordered table-hover',
+                'checkbox' => true,
+                'columns' => [
+                    ['id' => 'name', 'name' => trans('cms/datatables.name'), 'search' => true],
+                    ['id' => 'email', 'name' => trans('cms/datatables.email'), 'search' => true],
+                ],
+                'orderByColumn' => 0,
+                'order' => 'asc',
+                'buttons' => [
+                    [
+                        'url' => \Locales::route($this->route . '/create'),
+                        'class' => 'btn-primary js-create',
+                        'icon' => 'plus',
+                        'name' => trans('cms/forms.createButton')
+                    ],
+                    [
+                        'url' => \Locales::route($this->route . '/edit'),
+                        'class' => 'btn-warning disabled js-edit',
+                        'icon' => 'edit',
+                        'name' => trans('cms/forms.editButton')
+                    ],
+                    [
+                        'url' => \Locales::route($this->route . '/delete'),
+                        'class' => 'btn-danger disabled js-destroy',
+                        'icon' => 'trash',
+                        'name' => trans('cms/forms.deleteButton')
+                    ],
+                ]
+            ]
         ];
-    }
-
-    public function getDataTables($group)
-    {
-        $datatables = [];
-        if ($group) {
-            $datatables = [$group => $this->datatables[$group]];
-        } else {
-            if ($this->multipleDatatables) {
-                return array_except($this->datatables, [$this->route]);
-            } else {
-                return [$this->route => $this->datatables[$this->route]];
-            }
-        }
-
-        return $datatables;
     }
 
     /**
@@ -137,109 +129,21 @@ class UserController extends Controller {
      *
      * @return View
      */
-    public function index(User $user, Role $role, Request $request, $group = null, $internal = false)
+    public function index(DataTable $datatable, User $user, Role $role, Request $request, $group = null)
     {
-        if ($internal) {
-            $datatables = $this->getDataTables($group);
+        $routeParameter = \Locales::getDefaultParameter($this->route, $group);
+        if ($routeParameter) {
+            $user = $user->where('role_id', $role->select('id')->where('slug', $routeParameter)->firstOrFail()->id);
         } else {
-            $routeParameter = \Locales::getDefaultParameter($this->route, $group);
-            $datatables = $this->getDataTables($routeParameter);
+            $routeParameter = $this->route;
         }
 
-        if (!$this->multipleDatatables) {
-            $datatables[$group ?: $this->route]['url'] = \Locales::route($this->route, $group);
-            $userRole = $group ? $role->select('id')->where('slug', $group)->firstOrFail()->id : false;
-            $count = $userRole ? $user->where('role_id', $userRole)->count() : $user->count();
-        }
+        $datatable->setup($user, $routeParameter, $this->datatables[$routeParameter]);
 
-        foreach ($datatables as $key => $value) {
-            if ($this->multipleDatatables) {
-                $datatables[$key]['url'] = \Locales::route($this->route, $key);
-                $userRole = $role->select('id')->where('slug', $key)->firstOrFail()->id;
-                $count = $userRole ? $user->where('role_id', $userRole)->count() : $user->count();
-            }
+        $datatables = $datatable->getTables();
 
-            if (!$internal && $request->ajax()) {
-                $datatables[$key]['reloadTable'] = true;
-
-                $datatables[$key]['draw'] = (int)$request->input('draw', 1);
-                $datatables[$key]['recordsTotal'] = $count;
-
-                $columns = array_column($datatables[$key]['columns'], 'id');
-                if ($datatables[$key]['checkbox']) {
-                    array_unshift($columns, 'id');
-                }
-                $sql = $user->select($columns);
-                $sql = $userRole ? $sql->where('role_id', $userRole) : $sql;
-
-                $column = $request->input('columns.' . $request->input('order.0.column') . '.data', $datatables[$key]['columns'][$datatables[$key]['orderByColumn']]['id']);
-                $dir = $request->input('order.0.dir', $datatables[$key]['order']);
-                $sql = $sql->orderBy($column, $dir);
-
-                if ($request->input('search.value')) {
-                    $sql = $sql->where(function($query) use ($request) {
-                        $query->where('name', 'like', '%' . $request->input('search.value') . '%')->orWhere('email', 'like', '%' . $request->input('search.value') . '%');
-                    });
-
-                    $sql2 = $sql;
-
-                    if ($request->input('length') > 0) { // All = -1
-                        if ($request->input('start') > 0) {
-                            $sql = $sql->skip($request->input('start'));
-                        }
-
-                        $sql = $sql->take($request->input('length'));
-                    }
-
-                    $datatables[$key]['search'] = true;
-                    $datatables[$key]['data'] = $sql->get();
-                    $datatables[$key]['recordsFiltered'] = $sql2->count();
-                } else {
-                    $datatables[$key]['recordsFiltered'] = $count;
-
-                    if ($request->input('length') > 0) { // All = -1
-                        if ($request->input('start') > 0) {
-                            $sql = $sql->skip($request->input('start'));
-                        }
-
-                        $sql = $sql->take($request->input('length'));
-                    }
-
-                    $datatables[$key]['data'] = $sql->get();
-                }
-
-                return response()->json($datatables);
-            } else {
-                if (!$internal) {
-                    $datatables[$key]['count'] = $count;
-
-                    $size = ($count <= 100 ? 'small' : ($count <= 1000 ? 'medium' : 'large'));
-                    $datatables[$key]['size'] = $size;
-                }
-
-                if ($count < \Config::get('datatables.clientSideLimit')) {
-                    $datatables[$key]['ajax'] = false;
-
-                    $columns = array_column($datatables[$key]['columns'], 'id');
-                    if ($datatables[$key]['checkbox']) {
-                        array_unshift($columns, 'id');
-                    }
-                    $sql = $user->select($columns);
-                    $sql = $userRole ? $sql->where('role_id', $userRole) : $sql;
-
-                    if (!$internal) {
-                        $sql = $sql->orderBy($datatables[$key]['columns'][$datatables[$key]['orderByColumn']]['id'], $datatables[$key]['order']);
-                    }
-
-                    $datatables[$key]['data'] = $sql->get();
-                } else {
-                    $datatables[$key]['ajax'] = true;
-                }
-            }
-        }
-
-        if ($internal) {
-            return $datatables;
+        if ($request->ajax()) {
+            return response()->json($datatables);
         } else {
             return view('cms.' . $this->route . '.index', compact('datatables'));
         }
@@ -282,12 +186,12 @@ class UserController extends Controller {
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(User $user, Role $role, CreateUserRequest $request)
+    public function store(DataTable $datatable, User $user, Role $role, CreateUserRequest $request)
     {
-        $userRole = $request->input('group') ? $role->select('id')->where('slug', $request->input('group'))->firstOrFail()->id : false;
-
+        $userRole = $role->select('id')->where('slug', $request->input('group'))->firstOrFail()->id;
         $group = ($request->input('table') == $this->route ? null : $request->input('table'));
-        $redirect = redirect(\Locales::route($this->route, $group));
+        $param = $group ? \Locales::getRouteParameters($this->route)[$group] : true;
+        $redirect = redirect(\Locales::route($this->route, $param));
 
         $newUser = User::create([
             'role_id' => $userRole,
@@ -301,14 +205,12 @@ class UserController extends Controller {
             $redirect->withSuccess([$successMessage]);
 
             if ($request->ajax()) {
-                $datatables = $this->index($user, $role, $request, $group, true);
-                $table = $request->input('table');
-                return response()->json([
-                    $table => [
-                        'updateTable' => true,
-                        'data' => (isset($datatables[$table]['data']) ? $datatables[$table]['data'] : false),
-                        'ajax' => $datatables[$table]['ajax']
-                    ],
+                $user = $user->where('role_id', $userRole);
+                $datatable->setup($user, $request->input('table'), $this->datatables[$request->input('table')], true);
+                $datatable->setOption('url', \Locales::route($this->route, $param));
+                $datatables = $datatable->getTables();
+
+                return response()->json($datatables + [
                     'success' => $successMessage,
                     'resetExcept' => ['group']
                 ]);
@@ -351,10 +253,14 @@ class UserController extends Controller {
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $user, Role $role, Request $request)
+    public function destroy(DataTable $datatable, User $user, Role $role, Request $request)
     {
         $group = ($request->input('table') == $this->route ? null : $request->input('table'));
-        $redirect = redirect(\Locales::route($this->route, $group));
+        $param = true;
+        if ($group) {
+            $param = \Locales::getRouteParameters($this->route)[$group];
+        }
+        $redirect = redirect(\Locales::route($this->route, $param));
         $count = count($request->input('id'));
 
         if ($count > 0 && $user->destroy($request->input('id'))) {
@@ -362,14 +268,14 @@ class UserController extends Controller {
             $redirect->withSuccess([$successMessage]);
 
             if ($request->ajax()) {
-                $datatables = $this->index($user, $role, $request, $group, true);
-                $table = $request->input('table');
-                return response()->json([
-                    $table => [
-                        'updateTable' => true,
-                        'data' => (isset($datatables[$table]['data']) ? $datatables[$table]['data'] : false),
-                        'ajax' => $datatables[$table]['ajax']
-                    ],
+                if ($group) {
+                    $user = $user->where('role_id', $role->select('id')->where('slug', $group)->firstOrFail()->id);
+                }
+                $datatable->setup($user, $request->input('table'), $this->datatables[$request->input('table')], true);
+                $datatable->setOption('url', \Locales::route($this->route, $param));
+                $datatables = $datatable->getTables();
+
+                return response()->json($datatables + [
                     'success' => $successMessage,
                     'closePopup' => true
                 ]);
@@ -425,14 +331,15 @@ class UserController extends Controller {
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Role $role, EditUserRequest $request)
+    public function update(DataTable $datatable, Role $role, EditUserRequest $request)
     {
         $user = User::findOrFail($request->input('id'))->first();
 
-        $userRole = $request->input('group') ? $role->select('id')->where('slug', $request->input('group'))->firstOrFail()->id : false;
+        $userRole = $role->select('id')->where('slug', $request->input('group'))->firstOrFail()->id;
 
         $group = ($request->input('table') == $this->route ? null : $request->input('table'));
-        $redirect = redirect(\Locales::route($this->route, $group));
+        $param = $group ? \Locales::getRouteParameters($this->route)[$group] : true;
+        $redirect = redirect(\Locales::route($this->route, $param));
 
         if ($user->update([
             'role_id' => $userRole,
@@ -440,33 +347,18 @@ class UserController extends Controller {
             'email' => $request->input('email'),
             'password' => bcrypt($request->input('password')),
         ])) {
-            $successMessage = trans('cms/forms.updatedSuccessfully', ['entity' => trans_choice('cms/forms.entityUsers' . ucfirst($request->input('group')), 1)]);
+            $successMessage = trans('cms/forms.updatedSuccessfully', ['entity' => trans_choice('cms/forms.entityUsers' . ucfirst($group), 1)]);
             $redirect->withSuccess([$successMessage]);
 
             if ($request->ajax()) {
-                $datatables = $this->index($user, $role, $request, $group, true);
-                $table = $request->input('table');
-                $response = [
-                    $table => [
-                        'updateTable' => true,
-                        'data' => (isset($datatables[$table]['data']) ? $datatables[$table]['data'] : false),
-                        'ajax' => $datatables[$table]['ajax']
-                    ],
+                $datatable->setup(($group ? $user->where('role_id', $role->select('id')->where('slug', $group)->firstOrFail()->id) : $user), $request->input('table'), $this->datatables[$request->input('table')], true);
+                $datatable->setOption('url', \Locales::route($this->route, $param));
+                $datatables = $datatable->getTables();
+
+                return response()->json($datatables + [
                     'success' => $successMessage,
                     'closePopup' => true
-                ];
-
-                if ($group && $group != $request->input('group')) {
-                    $datatables = $this->index($user, $role, $request, $request->input('group'), true);
-                    $table = $request->input('group');
-                    $response[$table] = [
-                        'updateTable' => true,
-                        'data' => (isset($datatables[$table]['data']) ? $datatables[$table]['data'] : false),
-                        'ajax' => $datatables[$table]['ajax']
-                    ];
-                }
-
-                return response()->json($response);
+                ]);
             }
         } else {
             $errorMessage = trans('cms/forms.editError', ['entity' => trans_choice('cms/forms.entityUsers' . ucfirst($request->input('group')), 1)]);
