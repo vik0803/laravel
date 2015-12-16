9b28eba039586e6ec23474090072879406f03bae
Dimitar Zlatev <zlatevbg@gmail.com>
2015-12-17 00:46:48 +0200

    Sanitize Form data before validation

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+    /**
+     * Sanitizing Form Data Before Validating
+     */
+    public function all()
+    {
+        $attributes = parent::all();
+
+        if (isset($attributes['slug'])) {
+            $attributes['slug'] = str_slug($attributes['slug']);
+        }
+
+        $this->replace($attributes);
+
+        return parent::all();
+
+    }
 }
